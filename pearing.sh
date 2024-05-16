fcc5e6b0e19b74a9c4be46d74b389e4650de8f13
Jeff Brown <124228321+JeffreyDeanBrown@users.noreply.github.com>
2024-05-16 13:16:09 -0500

    Add files via upload

---
+++ pearing.sh
@@ -0,0 +1,17 @@
+# pearing.sh [directory containing reads]
+# This script will separate illumina sequences into R1 and R2 reads, then merged the R1 and R2 samples one at a time using PEAR
+# written by Jeff Brown (brown.jeffrey.dean@gmail.com)
+# last updated 05-16-2024
+mkdir $1sequences_R1/
+mkdir $1sequences_R2/
+mkdir $1unassembled_PEAR/
+mkdir $1Merged/
+mv $1*R1_* $1sequences_R1/
+mv $1*R2_* $1sequences_R2/
+for f in $1sequences_R1/*;
+ do
+	echo $f;
+	echo $1sequences_R2/$(basename $f | head -c-13)*;
+	pear -f $f -r $1sequences_R2/$(basename $f | head -c-13)* -o $1unassembled_PEAR/$(basename $f | head -c-13);
+done
+mv $1unassembled_PEAR/*.assembled.fastq $1Merged/
---
+++ trim_bulk.sh
@@ -0,0 +1,22 @@
+# trim_script.sh [directory with FASTQ files]
+# This trims all of the fastq files in a directory and spits out a statistics page with all stats added together, as well as all the individual stats for each read
+# written by Jeff Brown (brown.jeffrey.dean@gmail.com)
+# last updated 05-16-2024
+for f in $1* ;
+do echo -e "------------------------------\n$(basename $f)";
+fastq_quality_trimmer -Q 33 -t 20 -l 130 -i $f -o data_trimmed/trim_$(basename $f) -v;
+done > Trim_Statistics.txt
+	awk '
+BEGIN {
+input=(0)
+output=(0)
+discarded=(0)
+print "Statistics for all data:"}
+/Input:/ {input=input+$2}
+/Output:/ {output=output+$2}
+/discarded/ {discarded=discarded+$2}
+END {
+print "\nInput: "input" reads.\nOutput: "output" reads.\nDiscarded: "discarded" (" (discarded/input)*100"%) too-short reads.\n \nStatistics for each trimmed sample: \n"
+}
+' Trim_Statistics.txt | cat - Trim_Statistics.txt > temp
+mv temp Trim_Statistics.txt
