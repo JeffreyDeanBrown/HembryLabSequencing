ded1d2601721ae7c1bea6f612de6df1bd3ffd086
Jeff Brown <124228321+JeffreyDeanBrown@users.noreply.github.com>
2024-05-27 17:07:36 -0500

    Update pearing.sh

--- pearing.sh
+++ pearing.sh
@@ -11,13 +11,15 @@
 mkdir $1sequences_R1/
 mkdir $1sequences_R2/
 mkdir $1unassembled_PEAR/
-mkdir $1Merged/
+mkdir $1Merged_PEAR/
 mv $1*R1_* $1sequences_R1/
 mv $1*R2_* $1sequences_R2/
 for f in $1sequences_R1/*;
  do
+ #name the files being merged
 	echo $f;
 	echo $1sequences_R2/$(basename $f | head -c-13)*;
+ #merge the files (pear -f forward/read.fastq -r reverse/read.fastq -o output/path/merged_and_unnassembled
 	pear -f $f -r $1sequences_R2/$(basename $f | head -c-13)* -o $1unassembled_PEAR/$(basename $f | head -c-13);
 done
-mv $1unassembled_PEAR/*.assembled.fastq $1Merged/
+mv $1unassembled_PEAR/*.assembled.fastq $1Merged_PEAR/
