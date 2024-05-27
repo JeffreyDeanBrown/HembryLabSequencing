ec753ece776640f6edbd45d2c751d7b09da57592
Jeff Brown <124228321+JeffreyDeanBrown@users.noreply.github.com>
2024-05-27 16:59:49 -0500

    Update pearing.sh

--- pearing.sh
+++ pearing.sh
@@ -1,9 +1,13 @@
 #!/usr/bin/env bash
-
+#
+# 
 # pearing.sh [directory containing reads]
+#
 # This script will separate illumina sequences into R1 and R2 reads, then merged the R1 and R2 samples one at a time using PEAR
 # written by Jeff Brown (brown.jeffrey.dean@gmail.com)
 # last updated 05-16-2024
+#
+#
 mkdir $1sequences_R1/
 mkdir $1sequences_R2/
 mkdir $1unassembled_PEAR/
